21ef6e1fa35c71f14e4ece919c9a7cee5dd832fe
Tamal Anwar <tamalanwaronline@gmail.com>
2019-03-03 14:34:29 +0600

    Added popup box with local state in hooks

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import './create.css';
 import PropTypes from 'prop-types';
 
 const Create = (props) => {
@@ -8,16 +9,21 @@ const Create = (props) => {
     let name = event.target.name.value.trim();
     if (name) {
       props.dispatch({ type: 'CREATE', name });
+      props.toggleModal(false);
       event.target.reset();
     }
   }
 
   return (
-    <div>
-      <form onSubmit={createItem}>
-        <input name="name" type="text" />
-        <button>Create</button>
-      </form>
+    <div class="modal">
+      <div className="modal-content">
+        <button onClick={() => props.toggleModal(false)}>Close</button>
+        <h1>Add a new item</h1>
+        <form onSubmit={createItem}>
+          <input name="name" type="text" />
+          <button>Create</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import Single from '../Single/Single';
 import Create from '../Create/Create';
 import PropTypes from 'prop-types';
 
 const Layout = ({ clothes }) => {
+  // Local State for modal
+  const [modal, toggleModal] = useState(false);
   return (
     <div className="layout">
-      <Create />
-      {clothes.map((item, index) => (
-        <Single key={index} name={item} />
-      ))}
+      {modal ? <Create toggleModal={toggleModal} /> : ''}
+      <h1>Laundry List</h1>
+      <button onClick={toggleModal}>Add New Item</button>
+      <div className="items-area">
+        {clothes.map((item, index) => (
+          <Single key={index} name={item} />
+        ))}
+      </div>
     </div>
   );
 };
